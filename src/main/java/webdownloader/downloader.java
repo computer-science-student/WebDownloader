672195df4a27dd611b020a0b8206c939dd1f49b6
computer-science-student <legorrej@oregonstate.edu>
2021-10-27 12:51:46 -0500

    Wow the web page downloader worked!

--- src/main/java/webdownloader/downloader.java
+++ src/main/java/webdownloader/downloader.java
@@ -1,10 +1,26 @@
 package webdownloader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class downloader {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+   public static void main(String args[]) throws IOException {
+      download("http://www.google.com");
+   }
+   public static void download(String urlString) throws IOException {
+      URL url = new URL(urlString);
+      try(
+         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+         BufferedWriter writer = new BufferedWriter(new FileWriter("page.html"));
+      ) {
+         String line;
+         while ((line = reader.readLine()) != null) {
+            writer.write(line);
+         }
+         System.out.println("Page downloaded.");
+      }
+   }
 }
