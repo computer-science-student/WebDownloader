1f5b76f3739bca7d04b3e667463c20982888a851
computer-science-student <legorrej@oregonstate.edu>
2021-11-09 19:43:37 -0600

    I copied the readdatalinebyline function from geeksforgeeks. The code  runs properly still, but I haven't tested the function to see if it works yet.

--- src/main/java/webdownloader/downloader.java
+++ src/main/java/webdownloader/downloader.java
@@ -98,4 +98,39 @@ public class downloader {
          System.out.println("Page downloaded.");
       }
    }
+   
+   
+// Java code to illustrate reading a
+// CSV file line by line taken from here:
+// https://www.geeksforgeeks.org/reading-csv-file-java-using-opencsv/
+public static void readDataLineByLine(String file)
+{
+
+	try {
+
+		// Create an object of filereader
+		// class with CSV file as a parameter.
+		FileReader filereader = new FileReader(file);
+
+		// create csvReader object passing
+		// file reader as a parameter
+		CSVReader csvReader = new CSVReader(filereader);
+		String[] nextRecord;
+
+		// we are going to read data line by line
+		while ((nextRecord = csvReader.readNext()) != null) {
+			for (String cell : nextRecord) {
+				System.out.print(cell + "\t");
+			}
+			System.out.println();
+		}
+	}
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+}
+
+   
+   
+   
 }
