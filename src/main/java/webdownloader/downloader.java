39eb3101d6ffff0bf39e22d1eb01fe4dac8e5d55
computer-science-student <legorrej@oregonstate.edu>
2021-11-09 23:21:55 -0600

    I now import java.io.*. I deleted all the other packages that made use of the java.io.somepackagename.

--- src/main/java/webdownloader/downloader.java
+++ src/main/java/webdownloader/downloader.java
@@ -1,17 +1,12 @@
 package webdownloader;
-//import modules for csv files
-import java.io.Reader;
 // The way to download a webpage was taken from here: https://www.tutorialspoint.com/Download-webpage-in-Java
 // The way I took input in from the user was taken from here: (TODO add credit here later).
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 // import modules for copying file.
-import java.io.File;
 import java.nio.file.Files;
+//import modules for csv files
+import java.io.*;
+
 
 public class downloader {
    public static void main(String args[]) throws IOException {
