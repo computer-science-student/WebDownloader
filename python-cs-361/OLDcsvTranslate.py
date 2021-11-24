b85d0ac8db3c3a55cb21df9d6248c6e18bc3621b
computer-science-student <legorrej@oregonstate.edu>
2021-11-23 19:46:10 -0600

    I added python-cs-361 directory for real now. All the code in it is not mine. The code in the python-cs-361 is from this project: https://github.com/Haru825/CS361-Project/blob/main/myTranslate.py

---
+++ python-cs-361/OLDcsvTranslate.py
@@ -0,0 +1,33 @@
+from google_trans_new import google_translator
+import sys, getopt
+import webbrowser
+import os
+import csv
+
+def csvtext(tatext, language):
+    translator = google_translator()
+    takentext= str(tatext)
+    lang=str(language)
+    out=translator.translate(tatext, language)
+    csv_columns=['Original Text','Language','Translation']
+    langdic={
+        "Original text":tatext,
+        "Language":lang,
+        "Translation":out
+    }
+    
+    with open('translate.csv', 'w') as f:
+        for key in langdic.keys():
+            f.write("%s, %s\n" % (key, langdic[key]))
+    
+
+if __name__ == "__main__":
+    indict = {}
+    with open('file.csv', mode='r') as csvFile:
+        reader=csv.reader(csvFile)
+        dict_from_csv={rows[0]:rows[1] for rows in reader}
+    ntext=dict_from_csv["Original"]
+    lang=dict_from_csv["Language"]
+
+    pie=csvtext(ntext.lstrip(" "),lang.lstrip(" "))
+    
